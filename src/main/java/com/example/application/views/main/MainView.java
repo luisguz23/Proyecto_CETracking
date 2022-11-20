97dee210f42c3642fc444ced37d115c9287fabac
start.vaadin.com <no-reply@vaadin.com>
2022-11-20 01:03:18 +0000

    Se funcionalidades para la conexión entre la página y el servidor

---
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -0,0 +1,13 @@
+package com.example.application.views.main;
+
+import com.vaadin.flow.component.dependency.JavaScript;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+
+@Route
+@JavaScript("frontend://mapa.js")
+public class MainView extends Div{
+    public MainView(){
+
+    }
+}
